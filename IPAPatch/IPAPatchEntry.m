6b6f6ffbc7cbf46210869939e3d686b320e7be1d
zyqhi <zyqhi2010@gmail.com>
2018-08-29 20:06:49 +0800

    Load FLEX when app startup

--- IPAPatch/IPAPatchEntry.m
+++ IPAPatch/IPAPatchEntry.m
@@ -8,15 +8,33 @@
 
 #import "IPAPatchEntry.h"
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 
 @implementation IPAPatchEntry
 
 + (void)load
 {
     // DO YOUR WORKS...
+    [self ph_loadExtension];
     
     // For Example:
-    [self for_example_showAlert];
+//    [self for_example_showAlert];
+}
+
++ (void)ph_loadExtension {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self ph_loadFLEX];
+    });
+}
+
++ (void)ph_loadFLEX {
+    Class theFLEXManagerCls = NSClassFromString(@"FLEXManager");
+    
+    SEL sharedManagerSelector = NSSelectorFromString(@"sharedManager");
+    id theFLEXManagerInstance = ((id (*)(id, SEL))[theFLEXManagerCls methodForSelector:sharedManagerSelector])(theFLEXManagerCls, sharedManagerSelector);
+    
+    SEL showExplorerSelector = NSSelectorFromString(@"showExplorer");
+    ((void (*)(id, SEL))[theFLEXManagerInstance methodForSelector:showExplorerSelector])(theFLEXManagerInstance, showExplorerSelector);
 }
 
 + (void)for_example_showAlert
